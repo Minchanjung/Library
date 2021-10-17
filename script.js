d649524381841a4b466bf5e6e4fbc26ea2240de1
Minchan Jung <minchanjung1@gmail.com>
2021-10-17 14:27:49 +0100

    styeld and added toggle button

--- script.js
+++ script.js
@@ -11,26 +11,26 @@ function addBookToLibrary(book) {
     allBooks.push(book);
 }
 
-function displayBooks(array) { //creates a card for every object in varable allBooks
-    let cardContainer = document.querySelector('.cardContainer');
-    for (i = 0; i < array.length; i++) {
-        let divContainer = document.createElement('div');
-        divContainer.className = "bookCard";
-        cardContainer.appendChild(divContainer);
-        let title = document.createElement('h1');
-        title.className = "title";
-        divContainer.appendChild(title)
-        let content1 = document.createElement('p');
-        content1.className = "content1";
-        divContainer.appendChild(content1);
-        let content2 = document.createElement('p');
-        content2.className = "content2"
-        divContainer.appendChild(content2);
-        title.textContent = `${array[i].title}`
-        content1.textContent = `By ${array[i].author}`
-        content2.textContent = `${array[i].numberOfPages} pages`
-    }
-}
+// function displayBooks(array) { //creates a card for every object in varable allBooks
+//     let cardContainer = document.querySelector('.cardContainer');
+//     for (i = 0; i < array.length; i++) {
+//         let divContainer = document.createElement('div');
+//         divContainer.className = "bookCard";
+//         cardContainer.appendChild(divContainer);
+//         let title = document.createElement('h1');
+//         title.className = "title";
+//         divContainer.appendChild(title)
+//         let content1 = document.createElement('p');
+//         content1.className = "content1";
+//         divContainer.appendChild(content1);
+//         let content2 = document.createElement('p');
+//         content2.className = "content2"
+//         divContainer.appendChild(content2);
+//         title.textContent = `${array[i].title}`
+//         content1.textContent = `By ${array[i].author}`
+//         content2.textContent = `${array[i].numberOfPages} pages`
+//     }
+// }
 
 function displayBooksTest(singleObject) { //creates a card for just the book submitted by the user
     let cardContainer = document.querySelector('.cardContainer');
@@ -45,7 +45,6 @@ function displayBooksTest(singleObject) { //creates a card for just the book sub
     trashButton.className = "trashButton";
     trashButton.innerHTML = '<img src="img/icons8-trash-48.png">'
     divContainer.appendChild(trashButton)
-    console.log(trashButton);
     trashButton.addEventListener("click", () => {
         trashButton.parentElement.remove();
         //console.log(singleObject.id);
@@ -69,9 +68,32 @@ function displayBooksTest(singleObject) { //creates a card for just the book sub
     content2.className = "content2";
     divContainer.appendChild(content2);
 
-    title.textContent = `${currentObject.title}`
-    content1.textContent = `By ${currentObject.author}`
-    content2.textContent = `${currentObject.numberOfPages} pages`
+    let toggleLabel = document.createElement('label');
+    toggleLabel.setAttribute('for', 'switch');
+    toggleLabel.className = "toggleLabel";
+    divContainer.appendChild(toggleLabel);
+
+    let toggleContainer = document.createElement('label');
+    toggleContainer.setAttribute('id', "switch");
+    toggleContainer.className = "switch";
+    toggleContainer.setAttribute('for', 'toggle');
+    divContainer.appendChild(toggleContainer);
+
+    let toggle = document.createElement('input');
+    toggle.setAttribute('type', 'checkbox');
+    toggle.setAttribute('id', "toggle");
+    toggleContainer.appendChild(toggle);
+    
+
+    let slider = document.createElement('span');
+    slider.className = 'slider';
+    toggleContainer.appendChild(slider);
+
+    title.textContent = `${currentObject.title}`;
+    content1.textContent = `By ${currentObject.author}`;
+    content2.textContent = `${currentObject.numberOfPages} pages`;
+    toggleLabel.textContent = "Read";
+    toggle.addEventListener('click', readToggle(currentObject.finished));
 }
 
 function openForm() {
@@ -92,9 +114,9 @@ function submitForm(v) {
         finished: document.getElementById('finished').value, 
         id: Math.floor(Math.random() * (100 - 1 + 1) + 1)
     }
-    //console.log(book);
-    allBooks.push(book);
-    //console.log(allBooks);
+    console.log(book);
+    addBookToLibrary(book);
+    console.log(allBooks);
     displayBooksTest(book);
     document.forms[0].reset();
 
@@ -110,4 +132,3 @@ function readToggle() {
 }
 
 readToggle.prototype = Object.create(book.prototype);
-
